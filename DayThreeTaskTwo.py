9998112fb9a7ff2f4aa81c1ba7bb8b627db408cd
miarobin <miawest.west@googlemail.com>
2021-12-03 17:01:03 +0000

    Updated Day Three Task Two - Thanks Ryan

--- DayThreeTaskTwo.py
+++ DayThreeTaskTwo.py
@@ -4,16 +4,15 @@ readings = []
 with open('DayThreeTaskFile.txt') as test:
     readings = [line.strip('\n') for line in test.readlines()]
 
+def criteria(temp, column, thing):
+    return list(filter(lambda a: re.search(rf'.{{{column}}}{thing}.{{{(len(temp[0])-column-1)}}}',a)!=None,temp))
+    #return [a for a in temp if re.search(rf'.{{{column}}}{thing}.{{{(len(temp[0])-column-1)}}}',a)!=None]
 
 def oFinder(temp, column):
     if len(temp) == 1:
         return temp[0]
 
-    if(sum([int(line[column]) for line in temp])>=len(temp)/2):
-        temp = list(filter(lambda a: re.search(rf'.{{{column}}}1.{{{(len(temp[0])-column-1)}}}',a)!=None,temp))
-
-    else:
-        temp = list(filter(lambda a: re.search(rf'.{{{column}}}0.{{{(len(temp[0])-column-1)}}}',a)!=None,temp))
+    temp = criteria(temp, column, 1 if(sum([int(line[column]) for line in temp])>=len(temp)/2) else 0)
 
     return oFinder(temp,column+1)
 
@@ -22,11 +21,7 @@ def co2Finder(temp, column):
     if len(temp) == 1:
         return temp[0]
 
-    if(sum([int(line[column]) for line in temp])<len(temp)/2):
-        temp = list(filter(lambda a: re.search(rf'.{{{column}}}1.{{{(len(temp[0])-column-1)}}}',a)!=None,temp))
-
-    else:
-        temp = list(filter(lambda a: re.search(rf'.{{{column}}}0.{{{(len(temp[0])-column-1)}}}',a)!=None,temp))
+    temp = criteria(temp, column, 1 if(sum([int(line[column]) for line in temp])<len(temp)/2) else 0)
 
     return co2Finder(temp,column+1)
 
