f6b36c345f5c8e1d38f941cdbf915b129bf80eaa
miarobin <miawest.west@googlemail.com>
2021-12-31 12:11:20 +0000

    Day nine commit with added style (thanks again Ryan)

--- DayNineTaskTwo.py
+++ DayNineTaskTwo.py
@@ -1,7 +1,7 @@
 from functools import reduce
 def collBasin(array):
-    grid = [[10 for i in range(len(array[0])+2)]] + [[10] + line + [10] for line in array] + [[10 for i in range(len(array[0])+2)]]
-    basins = [len(basin(grid, set(), i, j))  for i in range(1,len(grid)-1) for j in range(1,len(grid[0])-1) if grid[i][j]<grid[i+1][j] and grid[i][j]<grid[i][j+1] and grid[i][j]<grid[i][j+1] and grid[i][j]<grid[i-1][j] and grid[i][j]<grid[i][j-1]]
+    grid = [[10 for _ in range(len(array[0])+2)]] + [[10] + line + [10] for line in array] + [[10 for _ in range(len(array[0])+2)]]
+    basins = [len(basin(grid, set(), i, j))  for i in range(1,len(grid)-1) for j in range(1,len(grid[0])-1) if all(grid[i][j]<grid[a][b] for a, b in ((i+1, j), (i, j+1), (i-1, j), (i, j-1)))]
     
     return reduce(lambda x, y: x*y, sorted(basins)[-3:])
 
